b14e0505e5518d607d9a7cf43b879f61a54177d4
Aaron Miller <amiller@aps.org>
2024-01-14 22:44:07 +0000

    chore: use tailwind workspace presets

--- apps/home/tailwind.config.js
+++ apps/home/tailwind.config.js
@@ -4,11 +4,15 @@ const { join } = require('path');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    join(__dirname, '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'),
+    join(
+      __dirname,
+      '{src,pages,components,app}/**/*!(*.stories|*.spec).{js,jsx,ts,tsx,mdx,html}'
+    ),
     ...createGlobPatternsForDependencies(__dirname),
   ],
   theme: {
     extend: {},
   },
   plugins: [],
+  presets: [require('../../tailwind-workspace-presets.js')],
 };
